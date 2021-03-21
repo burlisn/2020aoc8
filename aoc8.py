2d4a265cf3e925f82cedd5e52d582844242466bf
nnb2p3 <nnb2p3@mst.edu>
2021-03-21 11:42:20 -0500

    Finished part 2

--- aoc8.py
+++ aoc8.py
@@ -1,8 +1,8 @@
 def main() -> None:
     #variable declaration
-    instruction_number: int = 0
+    instruction_number: int = 0 #the instruction number
     acc: int = 0 #the accumulator
-    instructions_ran: list(int) = [] #keeps a list of the instruction numbers already ran
+    instructions_ran: list[int] = [] #keeps a list of the instruction numbers already ran
     keep_running: bool = True #boolean to keep while loop of instructions running
 
     #open input text file and get input
@@ -12,21 +12,30 @@ def main() -> None:
     #obtain list of instructions
     instruction_list=input_text.split('\n')
 
-    #instruction while loop
+    #instruction while loop (part 2)
     while keep_running:
         if instruction_number not in instructions_ran:
             instructions_ran.append(instruction_number)
             instruction_number, acc = perform_instruction(instruction_list[instruction_number], instruction_number, acc)
         elif instruction_number in instructions_ran:
             keep_running = False
-    
+
+    # -------------- Part 2 -------------------
+    for i, inst in enumerate(instruction_list):
+        if inst[0:3] == "nop":
+            instruction_list[i]=instruction_list[i].replace("nop", "jmp")
+            if(perform_run(instruction_list)[1] == True):
+                acc = perform_run(instruction_list)[0]
+            instruction_list[i]=instruction_list[i].replace("jmp", "nop")
+        
+        elif inst[0:3] == "jmp":
+            instruction_list[i]=instruction_list[i].replace("jmp", "nop")
+            if(perform_run(instruction_list)[1] == True):
+                acc = perform_run(instruction_list)[0]
+            instruction_list[i]=instruction_list[i].replace("nop", "jmp")
+            
     #print results
     print(acc)
-
-    #testing
-    # for instruction in instruction_list:
-    #     instruction_parts=instruction.split()
-    
     
 #Function performs the instruction provided
 #Returns: new instruction number and accumulator value
@@ -52,6 +61,29 @@ def perform_instruction(instruction: str, instruction_number: int, acc: int) -> 
         instruction_number += 1
 
     return instruction_number, acc
+
+#Peforms 'run' of assembly code, checking whether it runs correctly (if the last instruction runs)
+#Returns: value of accumulator (if it ran properly), boolean whether it ran properly
+def perform_run(instruction_list: list[str]) -> tuple[int, bool]:
+    instruction_number: int =0
+    keep_running: bool = True
+    instructions_ran: list[int] = []
+    acc: int = 0
+
+    while keep_running:
+        if instruction_number == len(instruction_list):
+            return acc, True
+        elif instruction_number not in instructions_ran:
+            instructions_ran.append(instruction_number)
+            instruction_number, acc = perform_instruction(instruction_list[instruction_number], instruction_number, acc)
+        elif instruction_number in instructions_ran:
+            keep_running = False
+
+    return 0, False
+
+
+
+
 
 if __name__ == "__main__":
     main()
